37ab1140f8b75256b9a687a8482c01e57c8a0b1c
Ian-Carlo <112873760+Ian-Carlo@users.noreply.github.com>
2023-12-21 11:23:46 -0300

    Update beecrowd3039.java

--- beecrowd3039.java
+++ beecrowd3039.java
@@ -1,10 +1,37 @@
-package beecrowd3039;
+import java.io.IOException;
+ import java.util.Scanner;
+/**
+ * - Ian Carlo de Lima Pedroso
+ * - 11/08/2023 18:20:25
+ *      
+ *    
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int car = 00;
+        int bon = 00;
+        for(int c =0; c < n; c++){
+           
+            String line = sc.nextLine();
+            String list[] = line.split(" ");
+            
+            if(list[1].equals("F")){
+            	
+                bon++;
+            }
+            else{
+                car++;
+            }
+            
+            
+        }
+        System.out.println(car + " carrinhos");
+        System.out.println(bon + " bonecas");
 
-public class beecrowd3039 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    }
+ 
 }
